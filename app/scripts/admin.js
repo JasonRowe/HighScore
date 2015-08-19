366cf5dc52a6cff89ec4fabd79818a8905d7ccae
jason.rowe <jason.rowe@gmail.com>
2015-08-19 10:36:34 -0500

    Add initial create form
    Add simple form which writes submitted list name to debug output.

--- app/scripts/admin.js
+++ app/scripts/admin.js
@@ -1,8 +1,16 @@
 ﻿angular.module('admin', [])
   .controller('modifyCtrl', ModifyCtrl)
-  .controller('createCtrl', CreateCtrl);
+  .controller('createCtrl', ['$scope', CreateCtrl]);
 
-function CreateCtrl() {
+function CreateCtrl($scope) {
+  $scope.list = [];
+      $scope.listName = 'My List Name';
+      $scope.submit = function() {
+        if ($scope.listName) {
+          $scope.list.push(this.listName);
+          $scope.listName = '';
+        }
+      };
 }
 
 function ModifyCtrl() {
